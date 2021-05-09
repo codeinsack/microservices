7bd699b13b1ac65a29b956567fcfd600fdb36fb3
codeinsack <codeinsack@gmail.com>
2021-05-09 19:59:59 +0300

    update client

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -11,7 +11,7 @@ export default () => {
   }, []);
 
   const fetchPosts = async () => {
-    const response = await axios.get('http://localhost:4000/posts');
+    const response = await axios.get('http://localhost:4002/posts');
     setPosts(response.data);
   }
 
@@ -20,7 +20,7 @@ export default () => {
       <div className="card-body">
         <h3>{post.title}</h3>
         <CommentCreate postId={post.id} />
-        <CommentList postId={post.id} />
+        <CommentList comments={post.comments} />
       </div>
     </div>
   ));
--- blog/query/index.js
+++ blog/query/index.js
@@ -8,13 +8,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-})
-
-app.post('/posts', (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data;
 
@@ -22,14 +16,36 @@ app.post('/posts', (req, res) => {
   }
 
   if (type === 'CommentCreated') {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
 
-    const post = postId[postId];
-    post.comments.push({ id, content });
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
   }
 
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find(comment => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
+
   res.send({});
-})
+});
 
 app.listen(4002, () => {
   console.log('Listening on 4002');
