e4e9a0b9a2ae4712ab1a50c5fdafca5acf0a6827
codeinsack <codeinsack@gmail.com>
2021-05-11 12:35:09 +0300

    generate CommentModerated event

--- blog/moderation/index.js
+++ blog/moderation/index.js
@@ -5,12 +5,24 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {
-  const event = req.body;
-  axios.post('http://localhost:4000/events', event);
-  axios.post('http://localhost:4001/events', event);
-  axios.post('http://localhost:4002/events', event);
-  res.send({ status: 'OK' });
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      }
+    })
+  }
+
+  res.send({});
 });
 
 app.listen(4003, () => {
